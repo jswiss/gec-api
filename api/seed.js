40f29b0883adf7ed4c8503e33ed1f5d53be20797
Joshua Swiss <swissjoshua@gmail.com>
2016-10-29 12:47:39 +0100

    fixed paths to models in seed.js again again

--- api/seed.js
+++ api/seed.js
@@ -7,7 +7,7 @@ seeder.connect(configDB.url, function() {
   seeder.start(__dirname + '/migrations', [
     
     { path: './api/models/Project.js', name: 'Project', clear: false },
-    { path: '.api/models/School.js', name: 'School', clear: false }
+    { path: './api/models/School.js', name: 'School', clear: false }
   ], true);
   debugger;
 });
